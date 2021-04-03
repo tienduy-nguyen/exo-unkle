80156da0a96abdaa090b7c5ab3cf82401f453630
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-04-03 12:18:04 +0200

    feat(fontend-ts): update first result of tooltip

---
+++ frontend-ts/src/app/components/Tooltip.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+interface TooltipProps {
+  content: string;
+  direction?: 'top' | 'bottom' | 'left' | 'right';
+  delay?: number;
+  background?: string;
+  color?: string;
+}
+export const Tooltip: React.FC<TooltipProps> = ({
+  content,
+  direction = 'right',
+  delay = 200,
+  background = '#333',
+  color = '#fff',
+  children,
+}) => {
+  const [show, setShow] = useState(false);
+
+  const showTip = () => {
+    setTimeout(() => {
+      setShow(true);
+    }, delay || 0);
+  };
+
+  const hitTip = () => {
+    setTimeout(() => {
+      setShow(false);
+    }, delay || 0);
+  };
+
+  const psodoElement = ()=>{
+
+  }
+
+  return (
+    <div className="tooltip-wrapper" onMouseEnter={showTip} onMouseLeave={hitTip}>
+      <>{children}</>
+      {show && (
+        <>
+          {/* ::before */}
+         {direction ='right' ? (
+
+         )}
+
+
+
+          <div
+            className={`tooltip-tip ${direction}`}
+            style={{
+              background: `${background}`,
+              color: `${color}`,
+            }}
+          >
+            {content}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
