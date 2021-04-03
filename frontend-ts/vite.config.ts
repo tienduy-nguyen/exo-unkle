1871b84ecf5fbf86f99ed640c22134dcf798ea53
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-04-03 09:45:11 +0200

    feat(frontend-ts): setup eslint + prettier

--- frontend-ts/vite.config.ts
+++ frontend-ts/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslint from '@rollup/plugin-eslint';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { UserConfig } from 'vite';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+const config: UserConfig = {
+  plugins: [
+    { ...eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' }), enforce: 'pre' },
+    reactRefresh(),
+  ],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, 'src'),
+    },
+  },
+};
+
+export default config;
