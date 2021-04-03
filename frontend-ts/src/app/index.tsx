f63537637e9a8cad5f3166bff69d01aec073291c
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-04-03 09:50:48 +0200

    feat(frontend-ts): clear projects

---
+++ frontend-ts/src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
