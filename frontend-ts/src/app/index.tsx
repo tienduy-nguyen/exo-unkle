d8e30e6e5d8f1c9828bc90ab58dde734ffc67201
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-04-03 10:27:20 +0200

    feat(frontend-ts): add buttons for demo

--- frontend-ts/src/app/index.tsx
+++ frontend-ts/src/app/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-      </header>
+    <div className="main">
+      <div className="container small mt-5">
+        <h1 className="text-center">React tooltips demo</h1>
+        <div className="d-flex justify-between mt-2">
+          <button className="btn">Tooltip on top</button>
+          <button className="btn">Tooltip on bottom</button>
+          <button className="btn">Tooltip on left</button>
+          <button className="btn">Tooltip on right</button>
+        </div>
+      </div>
     </div>
   );
 }
