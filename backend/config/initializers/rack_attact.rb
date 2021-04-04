1215ea87f8d42f89afe771ad3907353913ae60d6
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-04-04 03:16:08 +0200

    feat(backend): using rack_attack for more secure app

---
+++ backend/config/initializers/rack_attact.rb
@@ -0,0 +1,12 @@
+# Provided that trusted users use an HTTP request header named APIKey
+Rack::Attack.safelist("mark any authenticated access safe") do |request|
+  # Requests are allowed if the return value is truthy
+  request.env["HTTP_APIKEY"] == "secret-string"
+end
+
+# Always allow requests from localhost
+# (blocklist & throttles are skipped)
+Rack::Attack.safelist("allow from localhost") do |req|
+  # Requests are allowed if the return value is truthy
+  "127.0.0.1" == req.ip || "::1" == req.ip
+end
